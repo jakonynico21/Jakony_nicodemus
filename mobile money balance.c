b6752300c0382bb2c17fa560c42bab1d53bc569e
jakonynico21 <jakonynicodemus@gmail.com>
2025-03-23 09:23:01 +0300

    Update mobile money balance.c

--- mobile money balance.c
+++ mobile money balance.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int initial_balance = 50000; // Initial balance
+    int initial_balance = 50000;   // Initial mobile money balance
     int withdrawal = 20000; // Amount to withdraw
 
     // Calculate the remaining balance
-    int remaining_balance = initial_balance - withdrawal;
+    int remaining_balance = initial_balance - withdrawal ;
 
-    // Display the remaining balance
-    printf("Your remaining balance is ugx%d\n", remaining_balance);
+    // This is the amount withdrawn
+    printf(" Yello, Jakony Nicodemus, You have withdrawn UGX %d\n", withdrawal);
+
+
+    // This is the remaining balance
+    printf("Your remaining Mobile Money balance is UGX %d\n", remaining_balance);
+
+    // This is a thank you message
+    printf("Thank you for using MTN MoMo\n");
 
     return 0;
 }
